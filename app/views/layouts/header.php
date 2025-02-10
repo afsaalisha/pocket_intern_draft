01ef7a8ddeb27b9933489462872fd4c49d4680a2
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2025-02-10 11:48:52 +0800

    vsc suruh

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desktop Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="/poshet/public/css/styles.css">
+    <link rel="stylesheet" href="/poshet/public/css/mobile.css" media="screen and (max-width: 768px)">
     <script src="/public/js/tabs.js"></script>
 
-    </head>
+</head>
+
 <body class="body1">
