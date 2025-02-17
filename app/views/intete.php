5f0b1c95babbcec3739065af7aef44112d2556e1
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2025-02-17 15:02:45 +0800

    some things just cant be fixed.

--- app/views/intete.php
+++ app/views/intete.php
@@ -11,32 +11,40 @@
     <div class="cardo">
         <h2 style="background-color: rgba(255, 127, 0, 0.1);">General Information</h2>
         <div class="row">
-            <div class="column">
+            <div class="intetecolumn">
                 <!-- General Information content here -->
+                <?php
+                // Dummy data for now
+                $brandName = "Coffee Street Cafe";
+                $merchantID = "406988355";
+                ?>
+                <h3>Brand/Business Name</h3>
+                <p><?php echo $brandName; ?></p>
+                <br>
+                <h3>Merchant ID</h3>
+                <p><?php echo $merchantID; ?></p>
             </div>
         </div>
     </div>
     <div class="cardo">
         <h2 style="background-color: rgba(255, 127, 0, 0.1);">Terminals Information</h2>
-        <div>
-            <div>
-                <table>
-                    <thead>
+        <div class="apath">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Terminal No.</th>
+                        <th>Terminal Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php for ($i = 1; $i <= 10; $i++): ?>
                         <tr>
-                            <th>Terminal No.</th>
-                            <th>Terminal Name</th>
+                            <td>Terminal <?php echo $i; ?></td>
+                            <td>Terminal Name <?php echo $i; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php for ($i = 1; $i <= 10; $i++): ?>
-                            <tr>
-                                <td>Terminal <?php echo $i; ?></td>
-                                <td>Terminal Name <?php echo $i; ?></td>
-                            </tr>
-                        <?php endfor; ?>
-                    </tbody>
-                </table>
-            </div>
+                    <?php endfor; ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
