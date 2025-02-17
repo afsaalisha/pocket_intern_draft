60ac1b9bce54b20239e0398c73f0046c0cb4f15d
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2025-02-17 15:52:04 +0800

    kosong msih

--- app/views/finance.php
+++ app/views/finance.php
@@ -1,6 +1,14 @@
 <?php require_once 'layouts/header.php'; ?>
 <?php require_once 'layouts/sidebar.php'; ?>
 
-<div class="content">
+<div class="content skroll">
     <h1 class="title">Finances</h1>
 </div>
+
+<div>
+
+</div>
+
+<?php require_once 'layouts/footer.php'; ?>
+
+<!-- scripts here -->
