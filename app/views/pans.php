8cd4a56f6688b2eeec7cb0b136c89da85f0cd459
afsaalisha <22ftt1508@student.pb.edu.bn>
2025-02-18 08:51:57 +0800

    rah

--- app/views/pans.php
+++ app/views/pans.php
@@ -1,8 +1,11 @@
 <?php require_once 'layouts/header.php'; ?>
 <?php require_once 'layouts/sidebar.php'; ?>
 
-<div class="content">
+<div class="container">
     <h1 class="title">Manage Payment Links</h1>
+    <span class="pepep">Payment Links</span>
+    <p class="description">Here you can manage your payment links, view details, and add new links easily.</p>
+    <span class="add-button" id="openModal"><i class="fa-solid fa-plus"></i> Add New Link</span>
 
     <table>
         <thead>
@@ -14,63 +17,37 @@
                 <th>Actions</th>
             </tr>
         </thead>
-        <tbody id="userTableBody">
+        <tbody>
             <tr>
                 <td>001</td>
                 <td>Coffee</td>
-                <td>
-                    <a href="https://bit.ly/DigitalFuture24" target="_blank" class="truncate">https://bit.ly/DigitalFuture24</a>
-                </td>
-                <td>
-                    <a href="https://pay.pocket.com.bn/payments/setCustomAmount/..." target="_blank" class="truncate">
-                        https://pay.pocket.com.bn/payments/setCustomAmount/eyJtZXJjaGFud...
-                    </a>
-                </td>
-                <td><button class="Details" onclick="openDetailsModal()">Details</button></td>
+                <td><a href="https://bit.ly/DigitalFuture24" target="_blank" class="truncate">https://bit.ly/DigitalFuture24</a></td>
+                <td><a href="https://pay.pocket.com.bn/payments/setCustomAmount/..." target="_blank" class="truncate">https://pay.pocket.com.bn/payments/setCustomAmount/...</a></td>
+                <td><button class="Details">Details</button></td>
             </tr>
             <tr>
                 <td>501</td>
                 <td>Shopify API</td>
-                <td>
-                    <a href="https://docs.google.com/document/d/1zfoote8u7zuB8GL8xbkgFDpBevb8soEpEwxJ4x0FYt8/edit?usp=sharing" target="_blank" class="truncate">
-                        https://docs.google.com/document/d/1zfoote8...
-                    </a>
-                </td>
-                <td>
-                    <a href="https://pay.pocket.com.bn/payments/setCustomAmount/..." target="_blank" class="truncate">
-                        https://pay.pocket.com.bn/payments/setCustomAmount/eyJtZXJjaGFud...
-                    </a>
-                </td>
-                <td><button class="Details" onclick="openDetailsModal()">Details</button></td>
+                <td><a href="https://docs.google.com/document/d/1zfoote8..." target="_blank" class="truncate">https://docs.google.com/document/d/1zfoote8...</a></td>
+                <td><a href="https://pay.pocket.com.bn/payments/setCustomAmount/..." target="_blank" class="truncate">https://pay.pocket.com.bn/payments/setCustomAmount/...</a></td>
+                <td><button class="Details">Details</button></td>
             </tr>
             <tr>
                 <td>001</td>
                 <td>Coffee</td>
-                <td>
-                    <a href="https://en.wikipedia.org/wiki/Rose" target="_blank" class="truncate">
-                        https://en.wikipedia.org/wiki/Rose
-                    </a>
-                </td>
-                <td>
-                    <a href="https://pocket-payv2.threeg.asia/payments/setCustomAmount/..." target="_blank" class="truncate">
-                        https://pocket-payv2.threeg.asia/payments/setCustomAmount/eyJtZXJjaGFud...
-                    </a>
-                </td>
-                <td><button class="Details" onclick="openDetailsModal()">Details</button></td>
+                <td><a href="https://en.wikipedia.org/wiki/Rose" target="_blank" class="truncate">https://en.wikipedia.org/wiki/Rose</a></td>
+                <td><a href="https://pocket-payv2.threeg.asia/payments/setCustomAmount/..." target="_blank" class="truncate">https://pocket-payv2.threeg.asia/payments/setCustomAmount/...</a></td>
+                <td><button class="Details">Details</button></td>
             </tr>
             <tr>
                 <td>001</td>
                 <td>Coffee</td>
-                <td>
-                    <a href="https://google.com" target="_blank" class="truncate">https://google.com</a>
-                </td>
-                <td>
-                    <a href="https://pocket-payv2.threeg.asia/payments/setCustomAmount/..." target="_blank" class="truncate">
-                        https://pocket-payv2.threeg.asia/payments/setCustomAmount/eyJtZXJjaGFud...
-                    </a>
-                </td>
-                <td><button class="Details" onclick="openDetailsModal()">Details</button></td>
+                <td><a href="https://google.com" target="_blank" class="truncate">https://google.com</a></td>
+                <td><a href="https://pocket-payv2.threeg.asia/payments/setCustomAmount/..." target="_blank" class="truncate">https://pocket-payv2.threeg.asia/payments/setCustomAmount/...</a></td>
+                <td><button class="Details">Details</button></td>
             </tr>
         </tbody>
     </table>
 </div>
+
+
